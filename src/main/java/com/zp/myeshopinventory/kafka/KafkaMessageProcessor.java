691d733b78ff80e825e0d755fc00610a0ced8bf1
zhoupeng <287214764@qq.com>
2020-11-19 12:51:10 +0800

    storm缓存预热，热点数据nginx降级处理

--- src/main/java/com/zp/myeshopinventory/kafka/KafkaMessageProcessor.java
+++ src/main/java/com/zp/myeshopinventory/kafka/KafkaMessageProcessor.java
@@ -8,6 +8,7 @@ import com.zp.myeshopinventory.spring.SpringContext;
 import com.zp.myeshopinventory.zk.ZookeeperSession;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -39,18 +40,21 @@ public class KafkaMessageProcessor implements Runnable {
             String message = new String(it.next().message());
             System.out.println("===============接收到消息：" + message);
 
-            // 首先将message转换成json对象
-            JSONObject messageJSONObject = JSONObject.parseObject(message);
+            if(!StringUtils.isEmpty(message)){
+                // 首先将message转换成json对象
+                JSONObject messageJSONObject = JSONObject.parseObject(message);
 
-            // 从这里提取出消息对应的服务的标识
-            String serviceId = messageJSONObject.getString("serviceId");
+                // 从这里提取出消息对应的服务的标识
+                String serviceId = messageJSONObject.getString("serviceId");
 
-            // 如果是商品信息服务
-            if ("productInfoService".equals(serviceId)) {
-                processProductInfoChangeMessage(messageJSONObject);
-            } else if ("shopInfoService".equals(serviceId)) {
-                processShopInfoChangeMessage(messageJSONObject);
+                // 如果是商品信息服务
+                if ("productInfoService".equals(serviceId)) {
+                    processProductInfoChangeMessage(messageJSONObject);
+                } else if ("shopInfoService".equals(serviceId)) {
+                    processShopInfoChangeMessage(messageJSONObject);
+                }
             }
+
         }
     }
 
@@ -71,45 +75,46 @@ public class KafkaMessageProcessor implements Runnable {
         // 你从一个课程里，还是学到的是里面围绕的讲解的一些核心的知识
         // 缓存架构：高并发、高性能、海量数据，等场景
 
-        String productInfoJSON = "{\"id\": 4, \"name\": \"iphone7手机\", \"updateTime\": \"2018-01-01 12:00:01\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1}";
-        ProductInfo productInfo = JSONObject.parseObject(productInfoJSON, ProductInfo.class);
+        if(productId != null){
+            String productInfoJSON = "{\"id\": 4, \"name\": \"iphone7手机\", \"updateTime\": \"2018-01-01 12:00:01\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1}";
+            ProductInfo productInfo = JSONObject.parseObject(productInfoJSON, ProductInfo.class);
 
-        // 将数据写入redis之前，先获取分布式锁
-        ZookeeperSession zookeeperSession = ZookeeperSession.getInstance();
-        zookeeperSession.acquireLock(productId);
+            // 将数据写入redis之前，先获取分布式锁
+            ZookeeperSession zookeeperSession = ZookeeperSession.getInstance();
+            zookeeperSession.acquireLock(productId);
 
-        // 获取到锁
-        // 先从redis中取出数据
-        ProductInfo productInfo1 = cacheService.getProductInfoFromRedis(productId);
-        if (productInfo1 != null) {
-            // 如果redis中数据不为空，判断更新时间
-            String updateTimeNow = productInfo.getUpdateTime();
-            String updateTimeRedis = productInfo1.getUpdateTime();
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime timeNow = LocalDateTime.parse(updateTimeNow, dtf);
-            LocalDateTime timeRedis = LocalDateTime.parse(updateTimeRedis, dtf);
+            // 获取到锁
+            // 先从redis中取出数据
+            ProductInfo productInfo1 = cacheService.getProductInfoFromRedis(productId);
+            if (productInfo1 != null) {
+                // 如果redis中数据不为空，判断更新时间
+                String updateTimeNow = productInfo.getUpdateTime();
+                String updateTimeRedis = productInfo1.getUpdateTime();
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                LocalDateTime timeNow = LocalDateTime.parse(updateTimeNow, dtf);
+                LocalDateTime timeRedis = LocalDateTime.parse(updateTimeRedis, dtf);
 
-            if (timeNow.compareTo(timeRedis) < 0) {
-                // 现有数据中的updateTime新于redis数据中的updateTime时，才更新redis
-                zookeeperSession.releaseLock(productId);
-                return;
+                if (timeNow.compareTo(timeRedis) < 0) {
+                    // 现有数据中的updateTime新于redis数据中的updateTime时，才更新redis
+                    zookeeperSession.releaseLock(productId);
+                    return;
+                }
             }
+
+            try {
+                // 模拟并发测试
+                Thread.sleep(10 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            cacheService.saveProductInfo2LocalCache(productInfo);
+            System.out.println("===================获取刚保存到本地缓存的商品信息：" + cacheService.getProductInfoFromLocalCache(productId));
+            cacheService.saveProductInfo2RedisCache(productInfo);
+
+            // 释放锁
+            zookeeperSession.releaseLock(productId);
         }
-
-        try {
-            // 模拟并发测试
-            Thread.sleep(10 * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        cacheService.saveProductInfo2LocalCache(productInfo);
-        System.out.println("===================获取刚保存到本地缓存的商品信息：" + cacheService.getProductInfoFromLocalCache(productId));
-        cacheService.saveProductInfo2RedisCache(productInfo);
-
-        // 释放锁
-        zookeeperSession.releaseLock(productId);
-
 
     }
 
@@ -131,11 +136,14 @@ public class KafkaMessageProcessor implements Runnable {
         // 你从一个课程里，还是学到的是里面围绕的讲解的一些核心的知识
         // 缓存架构：高并发、高性能、海量数据，等场景
 
-        String shopInfoJSON = "{\"id\": 1, \"name\": \"小王的手机店\", \"level\": 5, \"goodCommentRate\":0.99}";
-        ShopInfo shopInfo = JSONObject.parseObject(shopInfoJSON, ShopInfo.class);
-        cacheService.saveShopInfo2LocalCache(shopInfo);
-        System.out.println("===================获取刚保存到本地缓存的店铺信息：" + cacheService.getShopInfoFromLocalCache(shopId));
-        cacheService.saveShopInfo2RedisCache(shopInfo);
+        if(shopId != null){
+            String shopInfoJSON = "{\"id\": 1, \"name\": \"小王的手机店\", \"level\": 5, \"goodCommentRate\":0.99}";
+            ShopInfo shopInfo = JSONObject.parseObject(shopInfoJSON, ShopInfo.class);
+            cacheService.saveShopInfo2LocalCache(shopInfo);
+            System.out.println("===================获取刚保存到本地缓存的店铺信息：" + cacheService.getShopInfoFromLocalCache(shopId));
+            cacheService.saveShopInfo2RedisCache(shopInfo);
+        }
+
     }
 
 }
